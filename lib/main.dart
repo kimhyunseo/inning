0edc7a7995b0585d65815c13c6924a7428b684eb
DODAM <64620695+kimhyunseo@users.noreply.github.com>
2026-01-20 20:21:39 +0900

    fear: webp 변환

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.lightTheme, home: WelcomePage());
+    return MaterialApp(theme: AppTheme.lightTheme, home: HomePage());
   }
 }
