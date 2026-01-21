8e12fc2cfdaad975f742c0f1e93c1135cce0c7a5
joohyelim1208 <jhl01208@gmail.com>
2026-01-20 22:19:47 -0800

    feat: 위치정보 받아오기 진행

--- lib/page/welcome/welcome_page.dart
+++ lib/page/welcome/welcome_page.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:inning/core/app_color.dart';
 import 'package:inning/core/fonts.dart';
 import 'package:inning/page/welcome/widgets/profile_card.dart';
-import 'package:inning/page/welcome/widgets/profile_image_picker.dart';
-import 'package:inning/page/welcome/widgets/profile_name_field.dart';
-import 'package:inning/page/welcome/widgets/profile_team_select_field.dart';
 
 class WelcomePage extends StatelessWidget {
   @override
