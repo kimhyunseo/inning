e9693cd27f30bcb58c88e9542910821bc7d975a1
DODAM <64620695+kimhyunseo@users.noreply.github.com>
2026-01-21 18:01:27 +0900

    fix: welcome page ui 수정

--- lib/page/welcome/widgets/profile_card.dart
+++ lib/page/welcome/widgets/profile_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inning/core/app_color.dart';
-import 'package:inning/page/team_select/team_select_page.dart';
+import 'package:inning/core/app_shadow.dart';
 import 'package:inning/page/welcome/widgets/profile_image_picker.dart';
 import 'package:inning/page/welcome/widgets/profile_name_field.dart';
 import 'package:inning/page/welcome/widgets/profile_team_select_field.dart';
@@ -16,13 +16,7 @@ class ProfileCard extends StatelessWidget {
       decoration: BoxDecoration(
         color: AppColors.white,
         borderRadius: BorderRadius.circular(4),
-        boxShadow: [
-          BoxShadow(
-            color: Color(0x3FB2B2B2),
-            blurRadius: 16,
-            offset: Offset(0, 0),
-          ),
-        ],
+        boxShadow: const [AppShadows.defaultShadow],
       ),
       child: Column(
         children: [
